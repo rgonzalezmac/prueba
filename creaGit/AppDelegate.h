92b214856450bb659a2b01bb22aa74d3f1a91ab6
Ricardo Gonzalez <rgtzeuz@hotmail.com>
2015-11-10 08:42:28 -0600

    Initial Commit

---
+++ creaGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  creaGit
+//
+//  Created by Ricardo Gonzalez on 10/11/15.
+//  Copyright © 2015 Ricardo Gonzalez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ creaGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  creaGit
+//
+//  Created by Ricardo Gonzalez on 10/11/15.
+//  Copyright © 2015 Ricardo Gonzalez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
